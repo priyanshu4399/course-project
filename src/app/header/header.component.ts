5645b21b2e5ba6c13b83153d30bf1fdf41b44adb
Priyanshu <priyanshu4399@gmail.com>
2022-07-31 13:40:22 +0530

    added componenets

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector : 'app-header',
+    templateUrl : "./header.component.html",
+    styles : ["./header.component.css"]
+})
+export class HeaderComponent{}
---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[]=[
+    new Recipe('A test recipe','This is simply a test','https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Zm9vZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'),
+    new Recipe('A test recipe','This is simply a test','https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Zm9vZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60')
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
